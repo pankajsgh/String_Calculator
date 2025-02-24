ef87807084c73be186127594d2c23803466d7117
Pankaj Pratap Singh <pankaj@makeitlivemedia.com>
2025-02-24 12:30:23 +0530

    Step - 2
    Create add function for only one string count

--- main.dart
+++ main.dart
@@ -3,7 +3,12 @@
 //String Calculator
 //dode can be run in https://dartpad.dev/
 
+
 void main() {
+
+  int result = StringCalculator().add("4");
+
+  print(result);
 
 }
 
@@ -11,5 +16,24 @@ class StringCalculator {
 
   int add(String numbers) {
 
+    if (numbers.length < 2) {
+      if (numbers.isEmpty) {
+        return 0;
+      } else {
+        return convertToInt(numbers);
+      }
+    } else {
+
+      // need to write lgogic
+
+      return 0;
+
+    }
   }
+
+  int convertToInt(String num) {
+    return int.parse(num);
+  }
+
+
 }
